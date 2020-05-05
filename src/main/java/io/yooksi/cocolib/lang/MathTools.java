74f0994a2cd4ecd631d03d19ee6f9794824578f9
Matthew Cain <yooksi@zoho.com>
2020-05-06 00:07:32 +0200

    Add MathTools class

---
+++ src/main/java/io/yooksi/cocolib/lang/MathTools.java
@@ -0,0 +1,27 @@
+package io.yooksi.cocolib.lang;
+
+public class MathTools {
+
+	/** Prevent class instantiation */
+	private MathTools() {}
+
+	/**
+	 * Ensure the given value is within specified {@code min-max} range.
+	 *
+	 * @param value {@code int} value to process
+	 * @param min minimum allowed value
+	 * @param max maximum allowed value
+	 *
+	 * @return {@code int} value within specified range (inclusive).
+	 */
+	public static int getValueInRange(int value, int min, int max) {
+		return Math.min(Math.max(value, min), max);
+	}
+
+	/**
+	 * @return {@code true} if the given value is within specified {@code min-max} range.
+	 */
+	public static boolean isValueInRange(int value, int min, int max) {
+		return (value>= min) && (value<= max);
+	}
+}
