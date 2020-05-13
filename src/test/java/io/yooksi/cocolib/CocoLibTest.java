0d6baea6b9c628c1ae9931fe50cfe70a0a50e905
Matthew Cain <yooksi@zoho.com>
2020-05-13 19:36:30 +0200

    Remove redundant event method

--- src/test/java/io/yooksi/cocolib/CocoLibTest.java
+++ src/test/java/io/yooksi/cocolib/CocoLibTest.java
@@ -3,8 +3,6 @@ package io.yooksi.cocolib;
 import io.yooksi.cocolib.gui.GuiHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(CocoLibTest.MOD_ID)
 public class CocoLibTest {
@@ -12,11 +10,6 @@ public class CocoLibTest {
 	public static final String MOD_ID = "cocolib";
 
 	public CocoLibTest() {
-
 		MinecraftForge.EVENT_BUS.register(new GuiHandler());
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-	}
-
-	private void setup(final FMLCommonSetupEvent event) {
 	}
 }
